ed6377bb12c924b022ed01941c7d522ff7774f07
UwaisN7 <uwaisn73@gmail.com>
2025-07-25 11:28:51 +0200

    Added a main menu scene to get it done gon start playing around with bal

---
+++ Assets/sceneManager.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class sceneManager : MonoBehaviour
+{
+
+    public void Level1()
+    {
+        SceneManager.LoadSceneAsync ("Level 1");
+    }
+
+  
+}
