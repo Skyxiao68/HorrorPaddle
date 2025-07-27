62b81740100b948772fb8576858b47b24762f8dd
Skyxiao68 <ben.lai.3368@gmail.com>
2025-07-27 15:52:56 +0200

    movement, camera, are on the player. added on trigger for ball to hit
    the ball gets sent when it collides with the cube

---
+++ Assets/Scripts/Player/Player_Behaviour.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player_Behaviour : MonoBehaviour
+{
+    [Header("Moving")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    [Header("GroundCHeck")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float currentSpeed;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        currentSpeed = walkSpeed;
+        
+    }
+
+    void Update()
+    {
+        
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+      
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+      
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            currentSpeed = runSpeed;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+       
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/Trigger.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class ThrowableObject : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public float throwHeight = 2f;
+    public bool throwToTarget = false;
+    public Transform target;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("ThrowTrigger"))
+        {
+            if (throwToTarget && target != null)
+            {
+                ThrowObjectToTarget(target.position);
+            }
+            else
+            {
+                ThrowObject();
+            }
+        }
+    }
+
+    public void ThrowObject()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        GetComponent<Collider>().isTrigger = false;
+
+        Vector3 throwDirection = transform.forward;
+        Vector3 throwVelocity = throwDirection * throwForce;
+        throwVelocity.y = throwHeight;
+
+        rb.linearVelocity = throwVelocity;
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
+    public void ThrowObjectToTarget(Vector3 targetPosition)
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        GetComponent<Collider>().isTrigger = false;
+
+        Vector3 velocity = CalculateLaunchVelocity(transform.position, targetPosition, 45f);
+        rb.linearVelocity = velocity;
+        rb.isKinematic = false;
+        rb.useGravity = true;
+    }
+
+    private Vector3 CalculateLaunchVelocity(Vector3 start, Vector3 end, float angle)
+    {
+        float gravity = Physics.gravity.magnitude;
+        float horizontalDistance = Vector3.Distance(new Vector3(start.x, 0, start.z), new Vector3(end.x, 0, end.z));
+
+        float velocity = horizontalDistance / (Mathf.Sin(2 * angle * Mathf.Deg2Rad) / gravity);
+
+        Vector3 direction = end - start;
+        direction.y = 0;
+        direction.Normalize();
+
+        Vector3 launchVelocity = direction * velocity * Mathf.Cos(angle * Mathf.Deg2Rad);
+        launchVelocity.y = velocity * Mathf.Sin(angle * Mathf.Deg2Rad);
+
+        return launchVelocity;
+    }
+}
