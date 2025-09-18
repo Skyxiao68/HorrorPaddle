b8f45f97ccc95761e562bdfbce7b2b2988a7657a
UwaisN7 <uwaisn73@gmail.com>
2025-09-18 17:33:52 +0200

    Everyhting is merged and the stuff for voice set up all i need to do is
    code the easter egg and get a bunch of haru urara pictures

--- Assets/Scripts/UI Scripts/TutorialText.cs
+++ Assets/Scripts/UI Scripts/TutorialText.cs
@@ -6,48 +6,121 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class TutorialText : MonoBehaviour
 {
+    public PlayerInputController inputControl;
+    public float inputFire;
+    public GameObject bat;
+    public GameObject propBat;
+    public bool hasBat =false;
     public TextMeshProUGUI moveMentText;
-   
+    public bool backtrack = false;
     public GameObject tutTextObj;
+    public bool voiceOn = true ;
 
+    private AudioSource voiceSource;
+    public AudioClip letMeOut;
+    public bool letMeOutB = true;
 
-   
+    public AudioClip greeting;
+    public bool greetingB = true;
+
+    public AudioClip pickMeUp;
+    public AudioClip iAmPickedUp;
+    public bool pickMeUpB = true;
+
+    public AudioClip playWithBall;
+    public bool playWithBallB = true;
+
+    public AudioClip rules;
+    public bool rulesB= true;
+
+    public AudioClip haruUrara;
+    public bool haruUraraB = true;
+
+    public AudioClip batOldFriend;
+    public void Awake()
+    {
+        inputControl = new PlayerInputController();
+        voiceSource = GetComponent<AudioSource>();
+        bat.SetActive(false);
+    }
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
     public void OnTriggerEnter(Collider collision)
     {
+        if ( voiceOn == false)
+        {
+            return;
+        }
         tutTextObj.SetActive(true);
 
-        if (collision.CompareTag("Movement"))
+        if (collision.CompareTag("Movement")&& letMeOutB == true)
         {
-            moveMentText.text = "WASD to move and use your mouse to look around.Left stick to move and Right stick to look around(please stop walking to read these) When you pause please navigate the menues with your mouse";
+            // Let me out of here you bastards! This is not fair ! The Showman cheated, he knew i was too good for him. 
+
+            voiceSource.PlayOneShot(letMeOut);
+            letMeOutB =false;
+
         }
-        if (collision.CompareTag("Welcome"))
+        if (collision.CompareTag("Welcome")&& greetingB == true)
         {
-            moveMentText.text = "Welcome to Horror paddle prototype 1";
+          // Wow ! Hello there, you are one ugly piece of shit, you look dumb too, what kind of rat hole did you crawl out of. I supposeyou will have to do please come and pick me up i need to get my revenge.
+          voiceSource.PlayOneShot (greeting);
+            greetingB =false;
         }
-        if (collision.CompareTag("BatPickup"))
+        if (collision.CompareTag("BatPickup")&& pickMeUpB == true)
         {
-            moveMentText.text = "On your right is a bat go and pick it up! Use Left click/ Right trigger on controller to swing try it out";
+            // Alright you took your time coming here , come on now pick me up , come on press buttons or something pick me up, you seriously dont know what to do? Pick me up , come on , you can do it just spam every button in front of you come one pick me. What is taking you so long please pick me up, You seriously are not going to pick me up , Ok ok the button is the shooting one pick me up now.
+
+            voiceSource.PlayOneShot(pickMeUp);
+            inputFire = inputControl.Gameplay.Fire.ReadValue<float>();
+            if (inputFire == 1)
+            {
+                bat.SetActive(true);
+                propBat.SetActive(false);
+                voiceSource.PlayOneShot(iAmPickedUp);
+                // Well done! You can follow basic instructions , if it takes you this long to complete a simple task i'd hate to see which world you were dragged in from.
+             pickMeUpB =false;
+                hasBat = true;
+            }
+          
+
         }
-        if (collision.CompareTag("PlayWithBall"))
+        if (collision.CompareTag("PlayWithBall")&& playWithBallB == true )
         {
-            moveMentText.text = "In the next room is a ball, you are free to play around with it until you are comfortable, once you are done proceed to the door on your right, Left Shift/L3 on controller can be used to sprint";
+           // Sure you would love to play with some balls dont you. Your arms look so small it looks like you cant even hold me upright, we are soo losing against Rudolph i am going to become another trophy, why me , he cheated i deserve a second chance , dont look at me i bet you are too dumb to even solve the secret to this place we all have, you can be the first to not. Go on prove me right. 
+
+            voiceSource.PlayOneShot(playWithBall);
+            playWithBallB =false;
+
         }
-        if (collision.CompareTag("Rules"))
+        if (collision.CompareTag("Rules")&& rulesB == true)
         {
-            moveMentText.text = "The rules for now are simple, try to hit the ball into the quadrants marked as red whilst keeping it out of your blue quadrants, the first to score 5 points wins ";
+            //AAAAAAAhhhhhhh shit here we go again, i think i am going to be sick with all the swinging i am about to do because you do not looked too skilled to me;
+            voiceSource.PlayOneShot(rules);
+            rulesB =false;
         }
-        if (collision.CompareTag("LastMin"))
+        if (collision.CompareTag("LastMin")&& haruUraraB == true)
         {
-            moveMentText.text = "Use your positioning and mouse aim to control the ball your arc is predetermined to make it easier, and do not hold in the hit button your shot will be weaker wait for the hit window, the window is not present in this tutorial to get you used to hitting the ball, proceed to the door on your right when you ready";
+            // You figured it out , so what it was obvious i would be surpised if you did not, anyway the person who runs this place is a bit crazy obsessed with horses and cats and my people , His a real weirdo , where i come from we call these people weebs, he seems to think that this haru urara person is his daughter or something , these cat , donkey, horse ears or whatever is on me is really anoying, the creator of this place clearly lacks artistic skill, what a dumbass i should give him lessons. 
+            voiceSource.PlayOneShot(haruUrara);
+            haruUraraB =false;
         }
         if (collision.CompareTag("NextLevel"))
         {
-            moveMentText.text = "Good Luck!";
-            SceneManager.LoadSceneAsync(2);
+           SceneManager.LoadSceneAsync(2);
         }
-        if (collision.CompareTag("Removed"))
+        if (collision.CompareTag("Removed") && hasBat == true)
         {
-            moveMentText.text = ".";
+           // I am sorry girl , i did what i could , i promise  will make the showman pay for your family , Hey you quit staring or we will end up here when you lose this. 
+           voiceSource.PlayOneShot(batOldFriend);
+            hasBat = false;
         }
     }
 
